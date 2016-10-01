cafda5032af55d0b877f171c5dbb1a9c46cc67a3
Jiho Kim <hogajiho@kaist.ac.kr>
2016-10-01 22:49:53 +0400

    initialize

---
+++ src/threads/build/tests/threads/hello.d
@@ -0,0 +1,5 @@
+tests/threads/hello.o: ../../tests/threads/hello.c ../../lib/stdio.h \
+  ../../lib/debug.h ../../lib/stdarg.h ../../lib/stdbool.h \
+  ../../lib/stddef.h ../../lib/stdint.h ../../lib/kernel/stdio.h \
+  ../../tests/threads/tests.h ../../threads/init.h ../../devices/timer.h \
+  ../../lib/round.h
